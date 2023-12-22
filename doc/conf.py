969377015bcba8e4bd0c7bb14df7d2268cded62f
Robert Lieck <robert.lieck@durham.ac.uk>
2023-12-22 21:55:51 +0100

    add __init__ doc to class doc

--- doc/conf.py
+++ doc/conf.py
@@ -79,3 +79,6 @@ intersphinx_mapping = {}
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# In the main body of an autoclass directive, insert both the class’ and the __init__ method’s docstring
+autoclass_content = 'both'
