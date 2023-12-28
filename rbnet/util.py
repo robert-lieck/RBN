03956ced56812e1051dce47667d88c820783bcbd
Robert Lieck <robert.lieck@durham.ac.uk>
2023-12-28 15:39:08 +0100

    update normalize_non_zero

--- rbnet/util.py
+++ rbnet/util.py
@@ -1,9 +1,14 @@
+import warnings
+
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import random_split
 import pytorch_lightning as pl
 import matplotlib.pyplot as plt
+
+
+_no_value = object()
 
 
 class SequenceDataModule(pl.LightningDataModule):
@@ -37,7 +42,7 @@ class SequenceDataModule(pl.LightningDataModule):
 
 
 # from https://github.com/robert-lieck/pyulib
-def normalize_non_zero(a, axis=None, skip_type_check=False):
+def normalize_non_zero(a, axis=_no_value, make_zeros_uniform=False, skip_type_check=False, check_positivity=True):
     """
     For the given ND array (NumPy or PyTorch), normalise each 1D array obtained by indexing the 'axis' dimension if
     the sum along the other dimensions (for that entry) is non-zero. Normalisation is performed in place.
@@ -45,8 +50,16 @@ def normalize_non_zero(a, axis=None, skip_type_check=False):
     # check array type (NumPy/PyTorch)
     if isinstance(a, np.ndarray):
         lib = "numpy"
+        any = np.any
+        all = np.all
+        logical_not = np.logical_not
+        ones_like = np.ones_like
     elif isinstance(a, torch.Tensor):
         lib = "pytorch"
+        any = torch.any
+        all = torch.all
+        logical_not = torch.logical_not
+        ones_like = torch.ones_like
     else:
         raise TypeError(f"Not implemented for arrays of type {type(a)}")
     # check that dtype is float (in place division of integer arrays silently rounds)
@@ -55,29 +68,59 @@ def normalize_non_zero(a, axis=None, skip_type_check=False):
                 (lib == "pytorch" and not torch.is_floating_point(a))):
             raise TypeError(f"Cannot guarantee that normalisation works as expected on array of type '{a.dtype}'. "
                             f"Use 'skip_type_check=True' to skip this check.")
-    # normalise along last axis per default
-    if axis is None:
+    # check positivity
+    if check_positivity and any(a < 0):
+        raise ValueError("Some elements are negative")
+    # normalise over everything is axis is not provided
+    if axis is _no_value:
+        # if axis is not specified, keep old behaviour for compatibility
+        warnings.warn("Not passing an explicit value to 'axis' is deprecated and will result in an error in the "
+                      "future. The old behaviour of implicitly assuming the last axis is currently kept for "
+                      "compatibility. The former default value of 'None' now results in normalising over "
+                      "everything.", DeprecationWarning)
         axis = a.ndim - 1
+    elif axis is None:
+        # None normalises over everything
+        axis = tuple(range(len(a.shape)))
     # make axis a tuple if it isn't
     if not isinstance(axis, tuple):
         axis = (axis,)
-    # compute sum along axis, keeping dimensions
-    if lib == "numpy":
-        s = a.sum(axis=axis, keepdims=True)
-    if lib == "pytorch":
-        s = a.sum(dim=axis, keepdim=True)
-    # check for non-zero entries
-    non_zero = (s != 0)
-    if (lib == "numpy" and not np.any(non_zero)) or (lib == "pytorch" and not torch.any(non_zero)):
-        # directly return if there are no non-zero entries
-        return a
-    # construct an index tuple to select the appropriate entries for normalisation (the dimensions specified by axis
-    # have to be replaced by full slices ':' to broadcast normalisation along these dimensions)
-    if lib == "pytorch":
-        kwargs = dict(as_tuple=True)
-    else:
-        kwargs = {}
-    non_zero_arr = tuple(slice(None) if idx in axis else n for idx, n in enumerate(non_zero.nonzero(**kwargs)))
+
+    # helper function to compute sum and non-zero entries
+    def get_sum(a):
+        if lib == "numpy":
+            s = a.sum(axis=axis, keepdims=True)
+        if lib == "pytorch":
+            s = a.sum(dim=axis, keepdim=True)
+        non_zero = (s != 0)
+        # construct an index tuple to select the appropriate entries for normalisation (the dimensions specified by axis
+        # have to be replaced by full slices ':' to broadcast normalisation along these dimensions)
+        kwargs = dict(as_tuple=True) if lib == "pytorch" else {}
+        non_zero_arr = tuple(slice(None) if idx in axis else n for idx, n in enumerate(non_zero.nonzero(**kwargs)))
+        return s, non_zero, non_zero_arr
+
+    # compute sum and non-zero entries
+    s, non_zero, non_zero_arr = get_sum(a)
+
+    # handle zero entries
+    if not any(non_zero):
+        # all entries are zero
+        if make_zeros_uniform:
+            # replace a with uniform array
+            a = ones_like(a)
+            s = get_sum(a)
+        else:
+            # nothing to normalise: directly return
+            return a
+    elif not all(non_zero):
+        # some entries are zero
+        if make_zeros_uniform:
+            # create a uniform array, fill non-zero entries with those from a
+            new_a = ones_like(a)
+            new_a[non_zero_arr] = a[non_zero_arr]
+            a = new_a
+            s, non_zero, non_zero_arr = get_sum(a)
+
     # in-place replace non-zero entries by their normalised values
     a[non_zero_arr] = a[non_zero_arr] / s[non_zero_arr]
     # return array
